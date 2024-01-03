064ebd37b77b3c58122eea70b09f810d26ee5385
Juan David Ochoa Gaviria <jochoa.gaviria@hotmail.com>
2024-01-03 15:59:01 -0500

    feat(storage.service): implements storage service using secure cookies, use that persistence on home.component and phone-number.component

--- src/app/features/qr/components/phone-number/phone-number.component.ts
+++ src/app/features/qr/components/phone-number/phone-number.component.ts
@@ -1,12 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { LocalStorageService } from '../../../../core/services/storage.service';
 
 @Component({
   selector: 'app-phone-number',
   templateUrl: './phone-number.component.html',
   styleUrl: './phone-number.component.scss'
 })
-export class PhoneNumberComponent {
+export class PhoneNumberComponent implements OnInit {
 
   private PHONE_NUMBER_REGEX = new RegExp(/^(\+?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})$/);
   private NON_NUMBER_REGEX = new RegExp(/\D/g);
@@ -20,17 +21,29 @@ export class PhoneNumberComponent {
     ]
   });
 
+  constructor(private localStorageService: LocalStorageService) { }
+
+  ngOnInit(): void {
+    const storage = this.localStorageService.getPhoneNumberItem();
+    if (storage) {
+      this.phoneNumberCtrl.setValue(storage);
+    }
+  }
+
   @Output() validNumberEvent = new EventEmitter<string>();
   @Output() invalidNumberEvent = new EventEmitter<boolean>();
 
   onInputSubmit() {
     if (this.phoneNumberCtrl.valid) {
       this.SOME_TIME_WAS_VALID = true;
+      
       let phoneNumberValue = this.phoneNumberCtrl.value
       let phoneNumberCleaned = phoneNumberValue.replace(this.NON_NUMBER_REGEX, "");
       this.validNumberEvent.emit(phoneNumberCleaned);
+
     } else if (this.SOME_TIME_WAS_VALID) {
       this.SOME_TIME_WAS_VALID = false;
+
       this.invalidNumberEvent.emit(true);
     }
   }
--- src/app/features/qr/pages/home/home.component.ts
+++ src/app/features/qr/pages/home/home.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Injector, OnInit, effect, inject, signal } from '@angular/core';
+import { LocalStorageService } from '../../../../core/services/storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  phoneNumber: string = '';
+export class HomeComponent implements OnInit {
+  phoneNumber = signal<string>('');
+  storageKey = 'phoneNumber';
+
+  injector = inject(Injector);
+
+  constructor(private localStorageService: LocalStorageService) { }
+
+  ngOnInit(): void {
+    const storage = this.localStorageService.getPhoneNumberItem();
+    if (storage) {
+      this.phoneNumber.set(storage);
+    }
+    this.trackChanges();
+  }
+
+  trackChanges() {
+    effect(() => {
+      const phoneNumber = this.phoneNumber();
+      this.localStorageService.setPhoneNumberItem(phoneNumber);
+    }, { injector: this.injector });
+  }
 
   onValidNumberEvent(event: string) {
-    this.phoneNumber = event
+    this.phoneNumber.set(event);
   }
 
   onInvalidNumberEvent() {
-    this.phoneNumber = '';
+    this.phoneNumber.set('');
   }
 }
